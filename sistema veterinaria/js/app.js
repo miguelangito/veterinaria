eef5a76eab3e4b6537a7a71152696356ef57b8f8
miguelangito <154333013+miguelangito@users.noreply.github.com>
2024-01-12 08:35:46 -0500

    actualizado

---
+++ sistema veterinaria/js/app.js
@@ -0,0 +1,25 @@
+const nombre_propietario = document.querySelector("#name_person");
+const nombre_mascota = document.querySelector("#name_pet");
+const telefono_propietario = document.querySelector("#telefono_propietario");
+const dia_cita = document.querySelector("#date_cite");
+const hora_cita = document.querySelector("#time_cite");
+const sintomas = document.querySelector("#description");
+const panel_cita = document.querySelectorAll("#cite_panel span");
+
+
+document.querySelector("#btn_submit").addEventListener("click", (event) => {
+  event.preventDefault()
+  document.querySelector("#name_owner_cite").textContent = nombre_propietario.value;
+  document.querySelector("#name_pet_cite").textContent = nombre_mascota.value;
+  document.querySelector("#num_pet_cite").textContent =telefono_propietario.value;
+  document.querySelector("#cite_date").textContent = dia_cita.value;
+  document.querySelector("#time_date").textContent = hora_cita.value;
+  document.querySelector("#symptoms").textContent = sintomas.value;
+
+  panel_cita.forEach((panel) => { panel.style.opacity = 1 });
+
+})
+
+document.querySelector("#btn_eliminar").addEventListener("click", function () {
+  panel_cita.forEach((panel) => { panel.textContent = "informacion"; panel.style.opacity = 0; })
+})
